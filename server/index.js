f7b2cc86870d9372d5e63e562075e946901b2769
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2022-03-31 21:03:41 -0300

    add error treatment

--- server/index.js
+++ server/index.js
@@ -3,8 +3,19 @@ import { serverInstance } from './server.js'
 import { logger } from './util.js'
 
 const port = config.server.port
-const host = config.server.host
+// const host = config.server.host
 
 serverInstance
   .listen(port)
-  .on('listening', () => logger.info(`Sever run at http://${host}:${port}`))
+  // .on('listening', () => logger.info(`Sever run at http://${host}:${port}`))
+  .on('listening', () => logger.info(`Sever run at port ${port}`))
+
+process.on('uncaughtException', (err) => {
+  logger.error(`Uncaught exception: ${err.stack || err}`)
+  // process.exit(1)
+})
+
+process.on('unhandledRejection', (err) => {
+  logger.error(`Unhandled rejection: ${err.stack || err}`)
+  // process.exit(1)
+})
